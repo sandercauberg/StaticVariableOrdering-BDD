7b2641070af11980d865bf5a26a0e0a1eb169cb2
sandercauberg <38723460+sandercauberg@users.noreply.github.com>
2024-02-27 10:08:42 +0100

    Update random.py

--- heuristics/random.py
+++ heuristics/random.py
@@ -2,7 +2,7 @@ import random
 
 
 def random_order(formula):
-    vars = formula.extract_variables()
+    vars = list(formula.extract_variables())
 
     random_order = random.sample(vars, len(vars))
     result_string = " < ".join(map(str, random_order))
