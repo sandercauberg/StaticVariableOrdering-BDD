dc1c4f7f2e61cc8957fb59025e2cc3d5b53fda2e
sandercauberg <38723460+sandercauberg@users.noreply.github.com>
2024-03-07 11:06:49 +0100

    Update weight heuristics with inherited methods/consistency

--- heuristics/bc_weight_heuristics.py
+++ heuristics/bc_weight_heuristics.py
@@ -1,38 +1,21 @@
+from meta.circuit import CustomCircuit
+
+
 def bc_weight_heuristics(circuit):
-    circuit.inputs = circuit.inputs()
-    circuit.output_gates = circuit.outputs()
+    circuit.inputs = CustomCircuit.get_ordered_inputs(circuit)
+    circuit.output_gates = CustomCircuit.get_ordered_outputs(circuit)
     input_weights = {input_var: 0 for input_var in circuit.inputs}
 
-    def calculate_weights(node, visited=None):
-        if visited is None:
-            visited = set()
+    # Iterate through the input gates in the circuit
+    for node in circuit.inputs:
+        input_weights[node] = len(circuit.transitive_fanout(node))
 
-        get_node = circuit.graph.nodes.get(node)
+    # print("Final input weights:", input_weights)
 
-        if node in visited or get_node.get("output"):
-            return 0
-
-        visited.add(node)
-
-        fanout_nodes = circuit.fanout(node)
-
-        if get_node.get("type") == "input":
-            weight = 0
-        else:
-            weight = 1
-        for successor in fanout_nodes:
-            weight += calculate_weights(successor, visited)
-
-        return weight
-
-    # Iterate through the output gates in the circuit
-    for gate in circuit.inputs:
-        input_weights[gate] = calculate_weights(gate, set())
-
-    print("Final input weights:", input_weights)
-
-    # Sort the input variables based on their weights in decreasing order
-    sorted_inputs = sorted(circuit.inputs, key=lambda x: -input_weights[x])
+    # Sort the input variables by weights primarily and input order secondarily
+    sorted_inputs = sorted(
+        circuit.inputs, key=lambda x: (-input_weights[x], circuit.inputs.index(x))
+    )
 
     # Create a result string based on the sorted input variables
     result_string = " < ".join(map(str, sorted_inputs))
