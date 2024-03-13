2267b6426d6ca043c2083e9bb14df8f33d7d5a8d
sandercauberg <38723460+sandercauberg@users.noreply.github.com>
2024-03-13 09:20:53 +0100

    Finish CNF to BC conversion by factoring out variable

--- helpers/cnf2bc.py
+++ helpers/cnf2bc.py
@@ -1,5 +1,5 @@
 from meta.circuit import CustomCircuit
-from meta.formula import Not, And, Or
+from meta.formula import Not, And, Or, Variable
 
 
 def extract_literal(formula, literals):
@@ -25,7 +25,7 @@ def factor_out(formula, literal):
     for clause in formula.children:
         if literal in clause.children:
             positive_factors.append(
-                And(*(term for term in clause.children if term != literal))
+                Or(*(term for term in clause.children if term != literal))
             )
         elif Not(literal) in clause.children:
             negative_factors.append(
@@ -50,17 +50,37 @@ def cnf2bc(cnf_formula):
     literal = literals.pop(0)
 
     formula = factor_out(cnf_formula, literal)
-    to_bc(formula)
-
-
-def to_bc(formula):
     circuit = CustomCircuit()
     literals = formula.extract_variables()
     [circuit.add(f"var_{var}", node_type="input") for var in literals]
 
     for var in formula.extract_negated_variables():
-        circuit.add(f"var_¬{var}", "not", fanin=f"var_{var}")
+        circuit.add(f"not_var_{var}", "not", fanin=f"var_{var}")
+    bc = to_bc(formula, circuit)
+    return bc
 
-    print(formula)
-    # TODO: we now have positive inputs and negations for literals, now add
-    #  AND and OR gates accordingly.
+
+def to_bc(formula, circuit):
+    def build_circuit(formula):
+        if isinstance(formula, Variable):
+            return f"var_{formula}"
+        elif isinstance(formula, Not):
+            negated_variable = formula.child
+            return f"not_var_{negated_variable}"
+        elif isinstance(formula, Or):
+            if len(formula.children) < 2:
+                return build_circuit(next(iter(formula.children)))
+            gate_name = f"or_{id(formula)}"
+            fanins = [build_circuit(child) for child in formula.children]
+            circuit.add(gate_name, "or", fanins)
+            return gate_name
+        elif isinstance(formula, And):
+            if len(formula.children) < 2:
+                return build_circuit(next(iter(formula.children)))
+            gate_name = f"and_{id(formula)}"
+            fanins = [build_circuit(child) for child in formula.children]
+            circuit.add(gate_name, "and", fanins)
+            return gate_name
+
+    build_circuit(formula)
+    return circuit
