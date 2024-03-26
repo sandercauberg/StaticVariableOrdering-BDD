3d82c5be1e10d86767c8637b3f6716b94ba4f11d
sandercauberg <38723460+sandercauberg@users.noreply.github.com>
2024-03-26 11:27:41 +0100

    Allow CUDD installation through make

--- install_dd_cudd.sh
+++ install_dd_cudd.sh
@@ -9,20 +9,6 @@
 #
 # ./install_dd_cudd.sh
 #
-# This script is unnecessary if you
-# want a pure-Python installation of `dd`.
-# If so, then `pip install dd`.
-#
-# This is script is unnecessary also
-# if a wheel file for your operating system
-# and CPython version is available on PyPI.
-# Wheel files (`*.whl`) can be found at:
-#     <https://pypi.org/project/dd/#files>
-#
-# If there *is* a wheel file on PyPI
-# that matches your operating system and
-# CPython version, then `pip install dd`
-# suffices.
 
 
 set -v
