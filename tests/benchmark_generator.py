bee7aec7f1ea20878e9bc3af21f53e0672f7bcfd
sandercauberg <38723460+sandercauberg@users.noreply.github.com>
2024-05-01 10:53:33 +0200

    Make benchmark script on two concurrent jobs

--- tests/benchmark_generator.py
+++ tests/benchmark_generator.py
@@ -1,14 +1,21 @@
+import argparse
+import concurrent.futures
 import os
+import sys
+
+sys.path.append("..")
 
 import pandas as pd
 
-import main
+from main import MyCLI
 
 # Define the list of commands to apply to each file, per category
 commands_dict = {
     "CNF": [
         "{}",
         "{} -heuristic fanin",
+        "{} -heuristic mince_manual",
+        "{} -heuristic mince",
         "{} -transform bc",
         "{} -transform bc -factor_out dependencies",
         "{} -transform bc -heuristic weight",
@@ -26,61 +33,81 @@ commands_dict = {
     ],
 }
 
-# Specify the folder containing the files
-folder_path = os.path.abspath("../input_files/benchmark_test")
-chosen_input = "CNF"  # Change this to switch between lists of commands
-my_cli = main.MyCLI()
+parser = argparse.ArgumentParser(
+    description="Run benchmark script with concurrent jobs option."
+)
+parser.add_argument(
+    "-j", "--jobs", type=int, default=2, help="Number of concurrent jobs"
+)
+parser.add_argument(
+    "-f", "--folder", default="benchmark_test", help="Folder containing the files"
+)
+parser.add_argument(
+    "-c",
+    "--category",
+    choices=["CNF", "BC"],
+    default="BC",
+    help="Category (CNF or BC)",
+)
+args = parser.parse_args()
+
+folder_path = os.path.abspath("../input_files/" + args.folder)
+chosen_input = args.category
+my_cli = MyCLI()
 
 rows = []
-
 commands = commands_dict[chosen_input]
 
-# Iterate over the files in the folder
-for file_name in sorted(os.listdir(folder_path)):
-    file_path = os.path.join(folder_path, file_name)
-    bdd = None
-    file_row = [file_name, "", "", "", ""]
-    # We do not want to include sub-folders and files, so we check if it is a file
-    if os.path.isfile(file_path):
-        # Apply the list of commands to the current file
-        for command_index, command in enumerate(commands):
-            try:
-                formatted_command = command.format(file_path)
-                # Execute the command with the file name as an argument
-                result_dict = main.MyCLI.do_choose(my_cli, formatted_command, bdd=bdd)
-                bdd = dict(
-                    tree=result_dict["Result"]["BDD Info"]["BDD"]["tree"],
-                    roots=result_dict["Result"]["BDD Info"]["BDD"]["roots"],
-                )
-                if (
-                    command_index == 0
-                ):  # Only fill in these columns for the first command
-                    print("COMMAND index 0, formatted command:", formatted_command)
-                    file_row = [
-                        file_name,
-                        result_dict["Result"]["Parsing Time"],
-                        result_dict["Result"]["BDD Info"]["Original BDD creation time"],
-                        result_dict["Result"]["BDD Info"]["Original BDD size"],
-                    ]
+with concurrent.futures.ThreadPoolExecutor(max_workers=args.jobs) as executor:
+    # Iterate over the files in the folder
+    for file_name in sorted(os.listdir(folder_path)):
+        file_path = os.path.join(folder_path, file_name)
+        bdd = None
+        file_row = [file_name, "", "", "", ""]
+        # We do not want to include sub-folders and files, so we check if it is a file
+        if os.path.isfile(file_path):
+            # Apply the list of commands to the current file
+            for command_index, command in enumerate(commands):
+                try:
+                    formatted_command = command.format(file_path)
+                    # Execute the command with the file name as an argument
+                    result_dict = MyCLI.do_choose(my_cli, formatted_command, bdd=bdd)
+                    bdd = dict(
+                        tree=result_dict["Result"]["BDD Info"]["BDD"]["tree"],
+                        roots=result_dict["Result"]["BDD Info"]["BDD"]["roots"],
+                    )
+                    if (
+                        command_index == 0
+                    ):  # Only fill in these columns for the first command
+                        print("COMMAND index 0, formatted command:", formatted_command)
+                        file_row = [
+                            file_name,
+                            result_dict["Result"]["Parsing Time"],
+                            result_dict["Result"]["BDD Info"][
+                                "Original BDD creation time"
+                            ],
+                            result_dict["Result"]["BDD Info"]["Original BDD size"],
+                        ]
 
-                file_row.extend(
-                    [
-                        command,
-                        result_dict["Factor_out"]
-                        if chosen_input == "CNF" and "transform bc" in formatted_command
-                        else "-",
-                        result_dict["Result"]["Ordering Time"],
-                        result_dict["Result"]["BDD Info"][
-                            "Reordered BDD creation time"
-                        ],
-                        result_dict["Result"]["BDD Info"]["Reordered BDD size"],
-                    ]
-                )
-            except Exception as e:
-                # If an error occurs, add placeholders for the command's columns
-                file_row.extend([command + str(e), "", "", "", "", ""])
-        # Append the file_row list to the rows list
-        rows.append(file_row)
+                    file_row.extend(
+                        [
+                            command,
+                            result_dict["Factor_out"]
+                            if chosen_input == "CNF"
+                            and "transform bc" in formatted_command
+                            else "-",
+                            result_dict["Result"]["Ordering Time"],
+                            result_dict["Result"]["BDD Info"][
+                                "Reordered BDD creation time"
+                            ],
+                            result_dict["Result"]["BDD Info"]["Reordered BDD size"],
+                        ]
+                    )
+                except Exception as e:
+                    # If an error occurs, add placeholders for the command's columns
+                    file_row.extend([command + str(e), "", "", "", "", ""])
+            # Append the file_row list to the rows list
+            rows.append(file_row)
 
 # Define the column names for the DataFrame
 columns = [
