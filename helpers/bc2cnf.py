257dbb9591ca1567654b099220b630daddaadc69
sandercauberg <38723460+sandercauberg@users.noreply.github.com>
2024-06-05 09:35:56 +0200

    WIP add minimal assignments for BC->CNF

--- helpers/bc2cnf.py
+++ helpers/bc2cnf.py
@@ -4,8 +4,37 @@ import tempfile
 
 import circuitgraph as cg
 
+from helpers.cudd_helper import build_bdd_from_circuit
+from meta.circuit import CustomCircuit
+
 
 def bc2cnf(circuit):
+    var_order = CustomCircuit.get_ordered_inputs(circuit)
+    bdd, roots = build_bdd_from_circuit(circuit, var_order)
+
+    conjunction = bdd.false
+    for root in roots:
+        conjunction = bdd.apply("or", conjunction, root)
+
+    minimal_assignments = list()
+
+    def traverse(node, path):
+        if node == bdd.true:
+            assignment = [(var.var, value) for var, value in path.items()]
+            minimal_assignments.append(assignment)
+        elif node != bdd.false:
+            var = bdd.var(node.var)
+            path[var] = True
+            traverse(node.high, path)
+            path[var] = False
+            traverse(node.low, path)
+            del path[var]
+
+    traverse(conjunction, {})
+
+    for assignment in minimal_assignments:
+        print("Minimal Satisfying Assignment:", dict(assignment))
+
     cnf_formula, variables = cg.sat.cnf(circuit)
     cnf_formula_str = str(cnf_formula).split("from_string='")[1].split("')")[0]
     cnf_formula_str = cnf_formula_str.replace("\\n", "\n")
